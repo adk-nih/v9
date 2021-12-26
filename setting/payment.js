5e202408a007478d298c0bda3bfd6f94b96f7bad
adk-nih <85114620+adk-nih@users.noreply.github.com>
2021-12-26 07:59:28 +0700

    Update payment.js

--- setting/payment.js
+++ setting/payment.js
@@ -2,8 +2,8 @@ exports.payment = (prefix) => {
 return `╭──「 *PAYMENT / DONASI* 」
 │
 ├❏ Gopay : _Tuh Kak Scan Qrnya_
-├❏ Dana : _Tidak Tersedia_
-├❏ Ovo : _Tidak Tersedia_
+├❏ Dana : _088220195739_
+├❏ Ovo : _Tuh Kak Scan Qrnya_
 │
 ╰──`
 }
